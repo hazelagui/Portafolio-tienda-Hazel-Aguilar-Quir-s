c46088c3191bd799b675e1b35c0564dc7c5002e8
hazelagui <hazelaguilarquiros@gmail.com>
2025-09-17 20:38:56 -0600

    commit semana 1 y 2

---
+++ TiendaHazel/src/main/java/Tienda/demo/TiendaHazelApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaHazelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaHazelApplication.class, args);
+	}
+
+}
