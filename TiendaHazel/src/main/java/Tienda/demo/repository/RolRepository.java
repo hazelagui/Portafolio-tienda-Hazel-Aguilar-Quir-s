02080a4e6d504a42efae73560919fc8e64e11d28
hazelagui <hazelaguilarquiros@gmail.com>
2025-10-01 21:00:52 -0600

    semana 4

---
+++ TiendaHazel/src/main/java/Tienda/demo/repository/RolRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package Tienda.demo.repository;
+
+/**
+ *
+ * @author hazelagx
+ */
+public interface RolRepository {
+
+}
---
+++ TiendaHazel/src/main/java/Tienda/demo/service/CategoriaService.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Tienda.demo.service;
+import Tienda.demo.domain.Categoria;
+import Tienda.demo.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author hazelagx
+ */
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) {
+            return categoriaRepository.findByActivoTrue();
+        }
+        return categoriaRepository.findAll();
+    }
+}
