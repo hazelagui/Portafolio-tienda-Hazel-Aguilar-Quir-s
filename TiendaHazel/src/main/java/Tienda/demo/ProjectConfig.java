74bf14624a73de6d0f7f08a585342f1724aaa7c7
hazelagui <hazelaguilarquiros@gmail.com>
2025-11-17 14:20:53 -0600

    semana 9 con errores

--- TiendaHazel/src/main/java/Tienda/demo/ProjectConfig.java
+++ TiendaHazel/src/main/java/Tienda/demo/ProjectConfig.java
@@ -1,25 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package Tienda.demo;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Locale;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -29,13 +22,10 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
-/**
- *
- * @author hazelagx
- */
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
 
+    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
@@ -43,10 +33,11 @@ public class ProjectConfig implements WebMvcConfigurer {
         registry.addViewController("/multimedia").setViewName("multimedia");
         registry.addViewController("/iframes").setViewName("iframes");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/acceso_denegado").setViewName("acceso_denegado");
         registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
     }
 
-        /* El siguiente método se utilizar para publicar en la nube, independientemente  */
+    /* El siguiente método se utilizar para publicar en la nube, independientemente  */
     @Bean
     public SpringResourceTemplateResolver templateResolver_0() {
         SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
@@ -79,7 +70,7 @@ public class ProjectConfig implements WebMvcConfigurer {
         registro.addInterceptor(localeChangeInterceptor());
     }
 
-       //Bean para poder acceder a los messages.properties en código...
+    //Bean para poder acceder a los messages.properties en código...
     @Bean("messageSource")
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -88,21 +79,85 @@ public class ProjectConfig implements WebMvcConfigurer {
         return messageSource;
     }
 
-    @Value("${firebase.json.path}")
-    private String jsonPath;
+    public static final String[] PUBLIC_URLS = {
+        "/", "/index", "/fav/**", "/carrito/**", "/consultas/**", "/registro/**",
+        "/js/**", "/webjars/**", "/login", "/acceso_denegado"
+    };
 
-    @Value("${firebase.json.file}")
-    private String jsonFile;
+    public static final String[] ADMIN_URLS = {
+        "/producto/nuevo", "/producto/guardar", "/producto/modificar/**", "/producto/eliminar/**",
+        "/categoria/nuevo", "/categoria/guardar", "/categoria/modificar/**", "/categoria/eliminar/**",
+        "/usuario/nuevo", "/usuario/guardar", "/usuario/modificar/**", "/usuario/eliminar/**"
+    };
+
+    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
+        "/producto/listado", "/categoria/listado", "/usuario/listado"
+    };
+
+    public static final String[] USUARIO_URLS = {
+        "/facturar/carrito"
+    };
 
     @Bean
-    public Storage storage() throws IOException {
-        ClassPathResource resource = new ClassPathResource(jsonPath + File.separator + jsonFile);
-        try (InputStream inputStream = resource.getInputStream()) {
-            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
-            return StorageOptions.newBuilder()
-                    .setCredentials(credentials)
-                    .build()
-                    .getService();
-        }
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers(PUBLIC_URLS).permitAll()
+                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
+                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
+                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
+                .anyRequest().authenticated()
+        ).formLogin(form -> form // Configuración de formulario de login
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+        ).logout(logout -> logout // Configuración de logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+        ).exceptionHandling(exceptions -> exceptions // Manejo de excepciones
+                .accessDeniedPage("/acceso_denegado")
+        ).sessionManagement(session -> session // Configuración de sesiones
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+        );
+        return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Este método será reemplazado la siguiente semana
+    @Bean
+    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("juan")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails sales = User.builder()
+                .username("rebeca")
+                .password(passwordEncoder.encode("456"))
+                .roles("VENDEDOR")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("pedro")
+                .password(passwordEncoder.encode("789"))
+                .roles("USUARIO") // Consistent con tu configuración
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, sales, user);
+    }
+
 }
+
+
+
+
--- TiendaHazel/src/test/java/Tienda/demo/TiendaHazelApplicationTests.java
+++ TiendaHazel/src/test/java/Tienda/demo/TiendaHazelApplicationTests.java
@@ -2,10 +2,15 @@ package Tienda.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.google.cloud.storage.Storage;
 
 @SpringBootTest
 class TiendaHazelApplicationTests {
-
+    
+        @MockBean
+        private Storage storage;
+   
 	@Test
 	void contextLoads() {
 	}
