d99fb31a6c7f222819c5227340cd77a4b391430a
hazelagui <hazelaguilarquiros@gmail.com>
2025-12-03 15:49:16 -0600

    Semana 11

--- TiendaHazel/src/main/java/Tienda/demo/repository/RolRepository.java
+++ TiendaHazel/src/main/java/Tienda/demo/repository/RolRepository.java
@@ -1,14 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
 package Tienda.demo.repository;
 
-/**
- *
- * @author andre
- */
-public interface RolRepository {
+import Tienda.demo.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
 
 }
+
